3ef8c21f514e0839e9f868ea4d247573af6a96d5
kaza980 <anickeewa.iula@mail.ru>
2019-01-03 14:26:33 +0400

    change directories

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, { Component } from "react";
 import { Platform, StyleSheet, Text, View } from "react-native";
-import MainScreen from "./android/app/src/screens/MainScreen/MainScreen.js";
+import MainScreen from "./src/screens/MainScreen/MainScreen.js";
 
 const instructions = Platform.select({
   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
